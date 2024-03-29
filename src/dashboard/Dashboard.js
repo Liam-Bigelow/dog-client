3d3ae22938b00ddb39ec1182d0ad4e4938af5fdd
Liam Bigelow <liamdbigelow@gmail.com>
2024-03-28 22:47:28 -0600

    format

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -4,18 +4,18 @@ import Upcoming from "../upcoming/Upcoming"
 import "./Dashboard.css"
 
 const Dashboard = (props) => {
-  return (
-    <div className="dashboard">
-      <div>
-        <h4>Upcoming Bookings</h4>
-        <Upcoming />
-        <md-divider style={{ margin: "10px 0" }}></md-divider>
-        <h4>Recent Activity</h4>
-        <Activty />
-      </div>
-      <Pets />
-    </div>
-  )
+    return (
+        <div className="dashboard">
+            <div>
+                <h4>Upcoming Bookings</h4>
+                <Upcoming />
+                <md-divider style={{ margin: "10px 0" }}></md-divider>
+                <h4>Recent Activity</h4>
+                <Activty />
+            </div>
+            <Pets />
+        </div>
+    )
 }
 
 export default Dashboard
--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -3,19 +3,19 @@ import "./Layout.css"
 import Menu from "../menu/Menu"
 
 const Layout = () => {
-  return (
-    <div className="container">
-      <div className="layout">
-        <Menu></Menu>
-        <Outlet />
-      </div>
-      <div className="profile-nav">
-        <md-fab variant="primary" aria-label="profile">
-          <md-icon slot="icon">person</md-icon>
-        </md-fab>
-      </div>
-    </div>
-  )
+    return (
+        <div className="container">
+            <div className="layout">
+                <Menu></Menu>
+                <Outlet />
+            </div>
+            <div className="profile-nav">
+                <md-fab variant="primary" aria-label="profile">
+                    <md-icon slot="icon">person</md-icon>
+                </md-fab>
+            </div>
+        </div>
+    )
 }
 
 export default Layout
